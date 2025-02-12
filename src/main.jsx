6473097a38799d2424f98d10f930b43baedd1d78
lucassperanzini <rm98297@fiap.com.br>
2025-02-11 23:14:18 -0300

    reorganizei o Canvas para estar no main

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Canvas } from '@react-three/fiber'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App canvasSize={100} />
+    <Canvas camera={{position:[0,0,10], fov:30}}>
+        <App  />
+    </Canvas>
+
   </StrictMode>,
 )
